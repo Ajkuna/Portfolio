550782872cf393306251b1a2950916fb3db4ef8a
Lorena <lorenazotaj10@gmail.com>
2020-10-18 10:56:02 +0200

    Third commit
    Added:
    - footer section (to be modified);
    - scroll up button and animation.

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
   $(window).scroll(function() {
-    if(this.scrollY > 30) {
+    if (this.scrollY > 120) {
       $(`.navbar`).addClass("sticky");
     } else {
       $(`.navbar`).removeClass("sticky");
     }
+
+    if (this.scrollY > 400) {
+      $(`.scroll-up-btn`).addClass("show");
+    } else {
+      $(`.scroll-up-btn`).removeClass("show");
+    }
+  });
+
+  // slide up script
+  $(`.scroll-up-btn`).click(function() {
+    $('html').animate({scrollTop: 0})
   });
 
 
@@ -12,7 +23,23 @@ $(document).ready(function() {
   $('.menu-btn').click(function() {
     $('.navbar .menu').toggleClass("active");
     $('.menu-btn i').toggleClass("active");
-  })
+  });
+
+
+  // typing aniamtion script
+  var typed = new Typed(".typing", {
+    strings: ["Developer", "Student", "Designer"],
+    typeSpeed: 80,
+    backSpeed: 60,
+    loop: true
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: ["Developer", "Student", "Designer"],
+    typeSpeed: 80,
+    backSpeed: 60,
+    loop: true
+  });
 
 
   // Owl carousel script
